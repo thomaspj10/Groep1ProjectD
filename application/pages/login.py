a18ca16206f95f64bb6d3fa9429b2b478aa8aa4f
Thomas <thomaspj2003@gmail.com>
2022-04-19 17:00:29 +0200

    Create the login structure
    - Made the login page
    - Added functionality for authenticated pages

--- application/pages/login.py
+++ application/pages/login.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import pages.sidebar
 
 def create_page():
-    st.write("Hello World!")
+    # Handle when the user submits the form.
+    def submit():
+        email = st.session_state["email"]
+        password = st.session_state["password"]
+        
+        # TODO - Login check
+        
+        st.session_state["logged_in"] = True
+        st.session_state["authentication_level"] = 1
+        
+        del st.session_state["password"]
+    
+    # Render the form.
+    with st.form("login_form"):
+        st.write("Login")
+        
+        st.text_input("Email", type="default", key="email")
+        st.text_input("Password", type="password", key="password")
+        
+        st.form_submit_button("Login", on_click=submit)
