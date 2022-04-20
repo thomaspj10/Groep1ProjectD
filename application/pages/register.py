9a99211bf3d53ae1ed4a72539f3b54939ef489e3
Thomas <thomaspj2003@gmail.com>
2022-04-20 14:34:35 +0200

    Create the register page

--- application/pages/register.py
+++ application/pages/register.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+from hashlib import sha256
 
 def create_page():
     available_roles = [
@@ -9,20 +11,36 @@ def create_page():
     # Handle when the user submits the form.
     def submit():
         email = st.session_state["register_email"]
-        password = st.session_state["register_password"]
+        telephone = st.session_state["register_telephone"]
+        username = st.session_state["register_username"]
+        password = sha256((st.session_state["register_password"] + "1MPlGCnOwSywPTg5BXbZ").encode("utf-8")).hexdigest()
         role = available_roles.index(st.session_state["register_role"]) + 1
         
-        # TODO - Add the new user to the database.
+        # TODO - Replace this with the api from https://github.com/thomaspj10/Groep1ProjectD/pull/2
+        import sqlite3
+        conn = sqlite3.connect("./application/db.sqlite")
         
-        del st.session_state["register_email"]
+        # Add the new user to the database.
+        user_row = pd.DataFrame({
+            "username": username,
+            "password": password,
+            "email": email,
+            "telephone": telephone,
+            "authentication_level": role
+        }, index=[0])
+        user_row.to_sql("user", conn, if_exists="append", index=False)
+        
+        st.success("Account created successfully!")
+        
         del st.session_state["register_password"]
-        del st.session_state["register_role"]
     
     # Render the form.
-    with st.form("register_form"):
+    with st.form("register_form", clear_on_submit=True):
         st.write("Register")
         
         st.text_input("Email", type="default", key="register_email")
+        st.text_input("Phone Number", type="default", key="register_telephone")
+        st.text_input("Username", type="default", key="register_username")
         st.text_input("Password", type="password", key="register_password")
         st.selectbox("Role", available_roles, key="register_role")
         
