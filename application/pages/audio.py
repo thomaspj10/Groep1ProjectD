32856f7a93430188ae003b4235bc63292370ed65
Rayan <rayandebruin@gmail.com>
2022-04-20 16:42:10 +0200

    Implemented Requested Changes

--- application/pages/audio.py
+++ application/pages/audio.py
@@ -1,10 +1,6 @@
 from urllib import response
 import streamlit as st
-import requests
 
-def create_page(url):
-    # Gets the audio file from the URL
-    audio = requests.get(url).content
-
-    # Creates an audio player 
-    st.audio(audio)
+def create_audio(url):
+    # Creates an audio player with the given url to the .wav file
+    st.audio(url)
