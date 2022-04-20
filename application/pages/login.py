44bd861247f38f9670b53ff0cfe3197f0768444b
Thomas <thomaspj2003@gmail.com>
2022-04-20 14:28:08 +0200

    Create the login system

--- application/pages/login.py
+++ application/pages/login.py
@@ -1,18 +1,30 @@
 import streamlit as st
-import pages.sidebar
+import pandas as pd
+from hashlib import sha256
 
 def create_page():
     # Handle when the user submits the form.
     def submit():
         email = st.session_state["email"]
-        password = st.session_state["password"]
-        
-        # TODO - Login check
-        
-        st.session_state["logged_in"] = True
-        st.session_state["authentication_level"] = 1
-        
+        password = sha256((st.session_state["password"] + "1MPlGCnOwSywPTg5BXbZ").encode("utf-8")).hexdigest()
         del st.session_state["password"]
+        
+        # TODO - Replace this with the api from https://github.com/thomaspj10/Groep1ProjectD/pull/2
+        import sqlite3
+        conn = sqlite3.connect("./application/db.sqlite")
+        
+        # Check the email and password againsts the database.
+        users = pd.read_sql("SELECT * FROM user", conn)
+        for index, row in users.iterrows():
+            if row["email"] == email and row["password"] == password:
+                st.session_state["logged_in"] = True
+                st.session_state["authentication_level"] = 1
+                
+                st.success("Successfully logged in!")
+                
+                return
+            
+        st.error("The email or password is incorrect!")
     
     # Render the form.
     with st.form("login_form"):
