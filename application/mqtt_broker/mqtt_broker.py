7ffd528fb22c02bc4a69be3e24d73281ebd292c1
Thomas P <thomaspj2003@gmail.com>
2022-04-20 12:39:48 +0200

    Update mqtt_broker.py
    Change the protected variables and methods to private

--- application/mqtt_broker/mqtt_broker.py
+++ application/mqtt_broker/mqtt_broker.py
@@ -1,34 +1,35 @@
 from paho.mqtt import client as mqtt_client
 
-_BROKER = "95.217.2.100"
-_PORT = 1883
-_TOPIC = "chengeta/notifications"
-_USERNAME = "chengeta_user"
-_PASSWORD = "chengeta2022"
+__BROKER = "95.217.2.100"
+__PORT = 1883
+__TOPIC = "chengeta/notifications"
+__USERNAME = "chengeta_user"
+__PASSWORD = "chengeta2022"
 
-def _connect_mqtt() -> mqtt_client:
+def __connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker.")
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client("Groep1")
-    client.username_pw_set(_USERNAME, _PASSWORD)
+    client = mqtt
+    client.Client("Groep1")
+    client.username_pw_set(__USERNAME, __PASSWORD)
     client.on_connect = on_connect
-    client.connect(_BROKER, _PORT)
+    client.connect(__BROKER, __PORT)
     return client
 
 
-def _subscribe(client: mqtt_client) -> None:
+def __subscribe(client: mqtt_client) -> None:
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}`")
 
-    client.subscribe(_TOPIC)
+    client.subscribe(__TOPIC)
     client.on_message = on_message
 
 
 def run() -> None:
-    client = _connect_mqtt()
-    _subscribe(client)
+    client = __connect_mqtt()
+    __subscribe(client)
     client.loop_forever()
