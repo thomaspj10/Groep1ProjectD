80c45dde8641d7ca64d6e6d874933508cd442753
Thomas <thomaspj2003@gmail.com>
2022-05-17 21:11:20 +0200

    Implement the cookie feature

--- application/main.py
+++ application/main.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pages.download_event
+import utils.cookies as cookies
 
 st.set_page_config(
     page_title="Chengeta Dashboard",
     page_icon=":deer:",
     initial_sidebar_state="expanded"
 )
+
+cookies.initialize_cookes()
 
 if "event" in st.experimental_get_query_params():
     pages.download_event.create_page()
