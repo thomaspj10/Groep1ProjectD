68c68ce6e6a90bcf63bf440cdf6a29f28fa5153e
Rayan <rayandebruin@gmail.com>
2022-05-19 11:09:23 +0200

    Testing A different Approach On Showing the Recent Events

--- pages/recent_events.py
+++ pages/recent_events.py
@@ -38,4 +38,51 @@ def create_page():
         events.at[index, 'Probability'] = events.at[index, 'Probability'] + '%'
 
     # Shows the table with the events
-    st.dataframe(events)
+    # st.dataframe(events)
+
+    event_id_col, node_id_col, date_col, time_col, lat_col, lon_col, sound_type_col, probability_col, sound_col = st.columns(9)
+
+    event_id_col.subheader('Event ID')
+    node_id_col.subheader('Node ID')
+    date_col.subheader('Date')
+    time_col.subheader('Time')
+    lat_col.subheader('Latitude')
+    lon_col.subheader('Longitude')
+    sound_type_col.subheader('Sound type')
+    probability_col.subheader('Probability')
+    sound_col.subheader('Sound')
+
+    for index, event in events.iterrows():
+        with event_id_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Event ID'])
+            st.markdown('\n')
+        with node_id_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Node ID'])
+            st.markdown('\n')
+        with date_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Date'])
+            st.markdown('\n')
+        with time_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Time'])
+            st.markdown('\n')
+        with lat_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Latitude'])
+        with lon_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Longitude'])    
+        with sound_type_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Sound type'])  
+            st.markdown('\n')
+        with probability_col:
+            st.markdown('***')
+            st.text(events.at[index, 'Probability'])
+            st.markdown('\n')
+        with sound_col:
+            st.markdown('***')
+            st.audio(events.at[index, 'Sound'])    
